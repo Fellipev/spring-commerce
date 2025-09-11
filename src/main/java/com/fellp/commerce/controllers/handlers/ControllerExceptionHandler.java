c94e69ce40f90e99b0fc43046473d115aa973c69
Fellipev <fellipe2reis@gmail.com>
2025-09-11 14:00:11 -0300

    feat database exception, fix ProductDTO

--- src/main/java/com/fellp/commerce/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/fellp/commerce/controllers/handlers/ControllerExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.fellp.commerce.controllers.handlers;
 
 import com.fellp.commerce.dto.CustomError;
+import com.fellp.commerce.services.exceptions.DatabaseException;
 import com.fellp.commerce.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -16,7 +17,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        CustomError err = new CustomError(Instant.now(), status.value(), "Recurso não encontrado.", request.getRequestURI());
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
 
     }
